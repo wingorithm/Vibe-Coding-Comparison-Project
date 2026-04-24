14010df92487d311bcaada89ae69ac63d9a4435c
wingorithm <erwin.gunawan@binus.ac.id>
2026-04-25 00:43:24 +0700

    initial bootstrap

---
+++ src/main/java/wingorithm/ticketing/vibecoding/TicketingVcApplication.java
@@ -0,0 +1,13 @@
+package wingorithm.ticketing.vibecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingVcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingVcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wingorithm/ticketing/vibecoding/TicketingVcApplicationTests.java
@@ -0,0 +1,13 @@
+package wingorithm.ticketing.vibecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingVcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
